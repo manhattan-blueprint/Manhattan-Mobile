3f7fdf4172cfcee319a0535753c074079838c398
Andrei Nitu <an16228@my.bristol.ac.uk>
2018-10-28 23:19:32 +0000

    Add login screen

--- Blueprint/Blueprint/ViewController.swift
+++ Blueprint/Blueprint/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameInput: UITextField!
+    @IBOutlet weak var passwordInput: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        usernameInput.placeholder = "Username"
+        passwordInput.placeholder = "Password"
+        
+        loginButton.setTitle("Login", for: .normal)
+        loginButton.layer.cornerRadius = 4
     }
-
-
+    
+    @IBAction func userTappedLogin(_ sender: UIButton) {
+        loginButton.isUserInteractionEnabled = false
+        loginButton.setTitleColor(UIColor.green, for: .disabled)
+            
+        print(usernameInput.text ?? "User instance is null")
+        print(passwordInput.text ?? "pass instance is null")
+    }
+    
 }
 
