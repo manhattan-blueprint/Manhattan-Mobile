d7dd5bc1e5ed4d1ddbfabd38b2b50045b44b7b74
Jay Lees <jay.lees@me.com>
2019-03-03 13:13:02 +0000

    Update unit tests for account type

--- Blueprint/app/src/main/java/com/manhattan/blueprint/Model/API/ServerSession.java
+++ Blueprint/app/src/main/java/com/manhattan/blueprint/Model/API/ServerSession.java
@@ -1,6 +1,7 @@
 package com.manhattan.blueprint.Model.API;
 
 import com.google.gson.annotations.SerializedName;
+import com.manhattan.blueprint.Model.AccountType;
 import com.manhattan.blueprint.Model.TokenPair;
 
 public class ServerSession {
@@ -13,6 +14,12 @@ public class ServerSession {
 
     @SerializedName("account_type")
     private String accountType;
+
+    public ServerSession(String refreshToken, String accessToken, String accountType) {
+        this.refreshToken = refreshToken;
+        this.accessToken = accessToken;
+        this.accountType = accountType;
+    }
 
 
     public TokenPair getTokenPair() {
